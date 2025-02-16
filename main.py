7139d3ba429eff6adb3eeab477adc191db5dd7b5
Emagnu <ignisvictorious@gmail.com>
2025-02-16 21:09:58 +0000

    some minor alteration

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
+def read_item(item_id: int, q: str | None = None) -> dict[str, str | None]:
     return {"item_id": item_id, "q": q}
